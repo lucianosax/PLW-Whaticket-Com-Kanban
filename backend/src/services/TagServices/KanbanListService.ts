987f6073c90e4e1b39836f5187154de38689d588
lucianosax <lusaxofonista54@gmail.com>
2024-05-12 13:30:34 -0300

    full aplication

---
+++ backend/src/services/TagServices/KanbanListService.ts
@@ -0,0 +1,28 @@
+import { Op } from "sequelize";
+import Tag from "../../models/Tag";
+import Ticket from "../../models/Ticket";
+import TicketTag from "../../models/TicketTag";
+
+interface Request {
+  companyId: number;
+}
+
+const KanbanListService = async ({
+  companyId
+}: Request): Promise<Tag[]> => {
+  const tags = await Tag.findAll({
+    where: {
+      kanban: 1,
+      companyId: companyId,
+      // status: {
+      //   [Op.ne]: "closed",
+      // }
+    },
+    order: [["id", "ASC"]],
+    raw: true,
+  });
+  //console.log(tags);
+  return tags;
+};
+
+export default KanbanListService;
